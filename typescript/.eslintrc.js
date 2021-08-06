9970bca5082adf36ef59321b82733cfe847f5dff
Lars Baunwall <larslb@thinkability.dk>
2021-08-06 15:02:34 +0200

    Add configuration infrastructure

---
+++ typescript/.eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+};
