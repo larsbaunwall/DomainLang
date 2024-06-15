52e746028f61db8430f1291d5fe74e88d62edcfe
Lars Baunwall <larslb@thinkability.dk>
2024-06-15 22:39:22 +0200

    Also copy syntax js for monaco to work in GH pages

--- dsl/dlang/scripts/prepare.js
+++ dsl/dlang/scripts/prepare.js
@@ -18,5 +18,4 @@ const logError = (err) => {
 cp('./node_modules/monaco-editor-workers/dist/index.js', './static/monaco-editor-workers/index.js', logError);
 cp('./node_modules/monaco-editor-workers/dist/workers/editorWorker-es.js', './static/monaco-editor-workers/workers/editorWorker-es.js', logError);
 cp('./node_modules/monaco-editor-workers/dist/workers/editorWorker-iife.js', './static/monaco-editor-workers/workers/editorWorker-iife.js', logError);
-cp('./syntaxes', './static/syntaxes', { recursive: true }, logError);
 cp('./language-configuration.json', './static/language-configuration.json', logError);
