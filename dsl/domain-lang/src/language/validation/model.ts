4767c40e21770867819a0833cedf54076eaed193
Lars Baunwall <larslb@thinkability.dk>
2025-06-06 17:46:57 +0200

    Move validation into its own language folder

---
+++ dsl/domain-lang/src/language/validation/model.ts
@@ -0,0 +1,19 @@
+import { setInferredRelationshipTypes } from '../services/relationship-inference.js';
+import { extractNames } from './shared.js';
+import type { ValidationAcceptor } from 'langium';
+import type { Model } from '../generated/ast.js';
+
+function checkModelIntegrity(model: Model, accept: ValidationAcceptor): void {
+    const uniqueNames = new Set<string>();
+    let names = extractNames(model);
+    for(const {fqn, node} of names) {
+        const oldSize = uniqueNames.size;
+        uniqueNames.add(fqn);
+        if (uniqueNames.size === oldSize) {
+            accept('error', `This element is already defined elsewhere: '${fqn}'`, { node: node, property: 'name' });
+        }
+    }
+    setInferredRelationshipTypes(model);
+}
+
+export const modelChecks = [checkModelIntegrity]; 
